04d538198681f077638ba6ec1dd8fa9e9041e21f
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-11-10 21:36:10 +0100

    added description and price step

--- airbnb/app/components/modal/RentModal.tsx
+++ airbnb/app/components/modal/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from '../inputs/CountrySelect'
 import dynamic from 'next/dynamic'
 import Counter from '../inputs/Counter'
 import ImageUpload from '../inputs/ImageUpload'
+import Input from '../inputs/Input'
 
 enum STEPS {
   CATEGORY = 0,
@@ -27,6 +28,7 @@ export default function RentModal() {
 
   
   const [step, setSteps] = useState(STEPS.CATEGORY)
+  const [isLoading, setLoading] = useState(false)
 
     const rentModal = useRentModal()
 
@@ -193,6 +195,56 @@ if (step === STEPS.IMAGES) {
     </div>
   )
 }
+
+if (step === STEPS.DESCRIPTION) {
+  bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="How would you describe your place?"
+        subtitle="Short and sweet works best!"
+      />
+      <Input
+        id="title"
+        label="Title"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <hr />
+      <Input
+        id="description"
+        label="Description"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+}
+
+if (step === STEPS.PRICE) {
+  bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Now, set your price"
+        subtitle="How much do you charge per night?"
+      />
+      <Input
+        id="price"
+        label="Price"
+        formatPrice 
+        type="number" 
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+}
+
   return (
     <div>
        {
@@ -212,3 +264,4 @@ if (step === STEPS.IMAGES) {
     </div>
   )
 }
+
