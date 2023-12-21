33a0b0e7dbb5f2a1b1abfab81f01dc0af662b88c
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-12-22 00:52:16 +0100

    deploying error issue

--- airbnb/app/page.tsx
+++ airbnb/app/page.tsx
@@ -7,6 +7,8 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 interface HomeProps{
   searchParams: IListingsParams
 }
+
+export const dynamic = 'force-dynamic'
 export default async function Home({searchParams}: HomeProps) {
   const listings = await getListings(searchParams)
   const currentUser = await getCurrentUser();
